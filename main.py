0609b65ae9565eb98f2ed432e703da311fa2cfc4
wizardxxdx <101555271+wizardxxdx@users.noreply.github.com>
2022-06-07 01:36:24 -0700

    updating main.py

--- main.py
+++ main.py
@@ -3,7 +3,10 @@
 import subprocess
 
 interface = "wlan0"
+new_mac = "00:11:22:11:33:11"
 
-subprocess.call(f"ifconfig {interface} down")
-subprocess.call(f"ifconfig {interface} down")
-subprocess.call(f"ifconfig {interface} down")
+print(f"[+] Changing MAC address for {interface} to {new_mac}")
+
+# subprocess.call(f"ifconfig {interface} down")
+# subprocess.call(f"ifconfig {interface} hw ether ")
+# subprocess.call(f"ifconfig {interface} down")
