51fd83801b84e9e8f9fdb49ae31759cdfb6d736f
wizardxxdx <101555271+wizardxxdx@users.noreply.github.com>
2022-06-07 01:45:16 -0700

    updating main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ new_mac = "00:11:22:11:33:11"
 
 print(f"[+] Changing MAC address for {interface} to {new_mac}")
 
-# subprocess.call(f"ifconfig {interface} down")
-# subprocess.call(f"ifconfig {interface} hw ether ")
-# subprocess.call(f"ifconfig {interface} down")
+
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
+
