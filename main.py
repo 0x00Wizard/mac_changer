74afcdc6ba343c0cb96ad77bc7ed0ac56c55735e
wizardxxdx <101555271+wizardxxdx@users.noreply.github.com>
2022-06-07 02:07:52 -0700

    updating main.py

--- main.py
+++ main.py
@@ -3,6 +3,14 @@
 import subprocess
 import optparse
 
+
+def change_mac(interface, new_mac):
+    print(f"[+] Changing MAC address for {interface} to {new_mac}")
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
 parser = optparse.OptionParser()
 
 parser.add_option("-i", "--interface", dest="interface",
@@ -10,16 +18,6 @@ parser.add_option("-i", "--interface", dest="interface",
 parser.add_option("-m", "--mac", dest="new_mac",
                   help="New MAC address")
 
-parser.parse_args()
+(options, args) = parser.parse_args()
 
-
-interface = input("Enter interface > ")
-new_mac = "00:11:22:11:33:11"
-
-print(f"[+] Changing MAC address for {interface} to {new_mac}")
-
-
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
-
+change_mac(options.interface, options.new_mac)
