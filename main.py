83619469f126cc2783029676afa4b7fbae196bf0
wizardxxdx <101555271+wizardxxdx@users.noreply.github.com>
2022-06-08 17:50:33 -0700

    'updating main.py'

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def get_args():
                       help="interface to change its MAC address")
     parser.add_option("-m", "--mac", dest="new_mac",
                       help="New MAC address")
+    parser.add_option("-o", "--os", dest="os",
+                      help="Enter if mac or linux")
     (options, args) = parser.parse_args()
     if not options.interface:
         parser.error("[-] Please specify an interface, use --help for more info")
@@ -27,17 +29,21 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 
-options = get_args()
-
-
 def get_mac_address(interface):
     ifconfig_result = subprocess.check_output(["ifconfig", interface])
     mac_address_search = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
     if mac_address_search:
         return mac_address_search.group(0)
     else:
-        return print("[-] Could not find MAC address.")
+        print("[-] Could not read MAC address.")
 
 
+options = get_args()
 # change_mac(options.interface, options.new_mac)
-get_mac_address(options.interface)
+current_mac = get_mac_address(options.interface)
+print(f"Current Mac = {str(current_mac)}")
+
+current_mac = get_mac_address(options.interface)
+if current_mac == options.new_mac:
+    print(f"[+] MAC address was successfully changed to {current_mac}")
+
